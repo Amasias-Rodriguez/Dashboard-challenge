d7151a78f194cb6ccc5a4e37651efdaa15cb3bc6
Amasias-Rodriguez <rodamasias01@gmail.com>
2023-08-28 22:26:58 -0600

    files-d2

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>Social Media Dashboard</h1>
+            <p>Total Followers: 23,004</p>
+            <hr />
+            <div className="flex justify-between">
+                <p className="bg-Lime-Green">Dark Mode</p>
+                <input type="checkbox" />
+            </div>
+        </header>
+    )
+}
