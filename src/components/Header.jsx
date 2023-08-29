7bae17af9e96a26c7551d72fb054e712d7b25664
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-29 15:09:23 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
-
+import { useState, useEffect } from "react"
 export const Header = () => {
+    const [darkMode, setDarkMode] = useState(false)
 
     const handleClick = () => {
-        document.documentElement.classList.add("dark")
+        setDarkMode(!darkMode)
     }
 
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }, [darkMode])
 
     return (
-        <header className="bg-Very-Pale-Blue dark:bg-slate-800 h-[235px] rounded-b-[20px] pt-8 px-6">
-            <h1 className="text-Very-Dark-Blue text-2x1 font-bold mb-1">Social Media Dashboard</h1>
-            <p className="text-Dark-Grayish-Blue font-bold mb-6">Total Followers: 23,004</p>
+        <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue  h-[235px] rounded-b-[20px] pt-8 px-6">
+            <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2x1 font-bold mb-1">Social Media Dashboard</h1>
+            <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-6">Total Followers: 23,004</p>
             <hr className="bg-black mb-[19px]" />
             <div className="flex justify-between">
-                <p className=" text-Dark-Grayish-Blue font-bold">Dark Mode </p>
+                <p className=" text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold">Dark Mode </p>
                 <label htmlFor="darkmode" className="border relative bg-Toggle w-12 h-6 rounded-full overflow-hidden cursor-pointer p-[3px]">
                     <input onClick={handleClick} id="darkmode" type="checkbox" className="peer sr-only" />
                     <div className=" w-full h-full peer-checked:bg-Toggle-Gradient absolute top-0 left-0"></div>
