8a042616ea4106994ac8481f30e351ae8f0b5e81
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-30 22:10:52 -0600

    Update OverviewCard.jsx

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -1,7 +1,8 @@
-
+import facebookLogo from '../assets/icon-facebook.svg'
 export const OverviewCard = ({ user, audienceType, audience }) => {
     return (
         <article className="bg-Light-Grayish-Blue w-[326px] h-[216px] mb-4 rounded-[5px] mx-auto">
+            <img src={facebookLogo} alt="logo" />
             <p>{user}</p>
             <p>{audience}</p>
             <p>{audienceType}</p>
