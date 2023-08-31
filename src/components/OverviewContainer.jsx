e56a73f2c35be8be92f06c6a6fc1a9a96a4199ef
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-31 11:39:01 -0600

    Update OverviewContainer.jsx

--- src/components/OverviewContainer.jsx
+++ src/components/OverviewContainer.jsx
@@ -1,19 +1,52 @@
+import { OverviewCard, OverviewTodayCard } from './OverviewCards'
 import data from '../../data/data.json'
-import { OverviewCard } from './OverviewCard'
+
+const converNumbertoK = (number) => {
+    if (number >= 10000) {
+        number = number / 1000
+        return number + 'k'
+    }
+    return number
+}
 
 export const OverviewContainer = () => {
     return (
-        <section>
+        <section className="absolute top-[191px] tablet:top-[140px] desktop:top-[160px] left-0 right-0 flex gap-[30px] px-4 desktop:px-0 flex-wrap max-w-[1140px] mx-auto">
             {
-
                 data.overview.map(object =>
-                    <OverviewCard key={object.id}
+                    <OverviewCard
+                        key={object.id}
                         user={object.user}
-                        audicenteType={object.audienceType}
-                        audience={object.audience}
+                        network={object.network}
+                        audienceType={object.audienceType}
+                        audience={converNumbertoK(object.audience)}
+                        today={object.today}
+                        isUp={object.isUp}
                     />
                 )
             }
+        </section>
+    )
+}
+
+export const OverviewTodayContainer = () => {
+    return (
+        <section className='max-w-[1140px] mx-auto'>
+            <h2 className="text-2xl font-bold mb-[27px] text-Dark-Grayish-Blue ml-8 dark:text-White tablet:ml-14 desktop:ml-0">Overview - Today</h2>
+            <div className="flex flex-wrap gap-[30px] px-4 desktop:px-0">
+                {
+                    data['overview-today'].map(object =>
+                        <OverviewTodayCard
+                            key={object.id}
+                            network={object.network}
+                            statsType={object.statsType}
+                            stats={converNumbertoK(object.stats)}
+                            porcentage={object.porcentage}
+                            isUp={object.isUp}
+                        />
+                    )
+                }
+            </div>
         </section>
     )
 }
