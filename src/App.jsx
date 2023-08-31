3bb21bb07468f834bf7926a52eab900d540eb4a9
Amasias-Rodriguez <rodamasias01@gmail.com>
2023-08-31 12:43:23 -0600

    final files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { Header } from "./components/Header";
-import { OverviewContainer } from "./components/OverviewContainer";
+import { Header } from "./components/Header"
+import { OverviewContainer, OverviewTodayContainer } from "./components/OverviewContainers"
+
 function App() {
   return (
-    <>
+    <main className="max-w-[1440px] relative mx-auto">
       <Header />
-    </>
+      <OverviewContainer />
+      <OverviewTodayContainer />
+    </main>
   )
 }
 
